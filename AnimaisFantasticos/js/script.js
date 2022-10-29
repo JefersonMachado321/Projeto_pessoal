1376897afa3f80a17077fa37125ad5413e2633fb
Jeferson Machado <jefersonmachado321@gmail.com>
2022-10-29 08:52:29 -0300

    FINALIZADO_TOOLTIP

--- AnimaisFantasticos/js/script.js
+++ AnimaisFantasticos/js/script.js
@@ -6,9 +6,11 @@ import initAccordion from "./modules/accordion.js";
 
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
-initModal();
-initTabNav();
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
+initTabNav();
+initModal();
+initTooltip();
